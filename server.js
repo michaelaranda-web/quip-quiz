a651db5f5b08a44ad8a87d9dd4a38fd99f94a4e4
Michael Aranda <michaelaranda0.web@gmail.com>
2018-06-01 23:16:06 +0000

    Upgrade Express to v4. Set up routing. Creates Quiz index page.

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
+router.get('/quizzes', function(req, res) {
+  res.sendFile(getPage('/quizzes/index.html'));
+});
+
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
+
+function getPage(filename) {
+  return path.join(__dirname, 'client/views', filename);
+}
