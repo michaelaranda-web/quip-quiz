dee7020ed004ced2d81477555bbc8ffaaf0ab735
Michael Aranda <michaelaranda0.web@gmail.com>
2018-06-10 15:27:05 +0000

    Adds Sass.

--- client/javascript/index.js
+++ client/javascript/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from '../css/index.scss';
 
 class App extends React.Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,26 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.scss$/,
+          use: [
+            {
+              loader: "style-loader"
+            }, 
+            {
+              loader: "css-loader", 
+                options: {
+                  sourceMap: true
+                }
+            }, 
+            {
+              loader: "sass-loader", 
+                options: {
+                  sourceMap: true
+                }
+            }
+          ]
+      }
     ]
   }
 };
