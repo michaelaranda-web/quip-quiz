955d35f34e4542c260e34abc7fe7625bf00879cc
Michael Aranda <michaelaranda0.web@gmail.com>
2018-07-05 02:29:04 +0000

    Prevent quiz submission until choices are made for all questions.

--- client/javascript/components/QuizShowPage.js
+++ client/javascript/components/QuizShowPage.js
@@ -9,8 +9,9 @@ export class QuizShowPage extends React.Component {
     
     this.state = {
       quiz: null ,
-      responses: {}
-    }
+      responses: {},
+      showErrorMessage: false
+    };
     
     this.onChoiceSelect = this.onChoiceSelect.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -26,6 +27,14 @@ export class QuizShowPage extends React.Component {
         quiz: data
       })
     });
+  }
+  
+  renderErrorMessage() {
+    if (this.state.showErrorMessage) {
+      return (
+        <span className="error-message">Please select an answer for each question.</span>  
+      )
+    }
   }
   
   render() {
@@ -54,6 +63,7 @@ export class QuizShowPage extends React.Component {
             }
             
             <div className="buttons-row">
+              {this.renderErrorMessage()}
               <a className="button save-button" onClick={self.onSubmit}>Submit</a>
             </div>
           </div>
@@ -73,24 +83,33 @@ export class QuizShowPage extends React.Component {
   onSubmit(event) {
     event.preventDefault();
     
-    $.ajax({
-      url: `/api/quizzes/${this.state.quiz._id}/submit`,
-      type: 'POST',
-      data: {
-        responses: this.state.responses
-      },
-      success: function(data) {
-        sessionStorage.setItem('quiz_result_id', data.quizResultId);
-        
-        if (data.redirectPath) {
-          window.location.href = data.redirectPath;
-        } else {
-          console.log("success!");
-        }
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+    var numResponses = Object.keys(this.state.responses).length;
+    var numQuestions = this.state.quiz.questions.length;
+    
+    if (numResponses === numQuestions) {
+      $.ajax({
+        url: `/api/quizzes/${this.state.quiz._id}/submit`,
+        type: 'POST',
+        data: {
+          responses: this.state.responses
+        },
+        success: function(data) {
+          sessionStorage.setItem('quiz_result_id', data.quizResultId);
+          
+          if (data.redirectPath) {
+            window.location.href = data.redirectPath;
+          } else {
+            console.log("success!");
+          }
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    } else {
+      this.setState({
+        showErrorMessage: true
+      })
+    }
   }
 }
